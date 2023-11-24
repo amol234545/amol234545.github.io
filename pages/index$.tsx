bfca37bb0fcbc7d86f81c24d9c5ef76b7a39e023
amol234545 <amolmilton@gmail.com>
2023-11-24 18:29:10 -0500

    changed vite system to vite-plugin-react-pages and added bootstrap

---
+++ pages/index$.tsx
@@ -0,0 +1,22 @@
+/**
+ * @title Hello
+ */
+import React from "react";
+export default class Application extends React.Component {
+  render () {
+   return (
+     <>
+     <html>
+        <head>
+            <title>Hello</title>
+        </head>
+        <body>
+            <div>
+                
+            </div>
+        </body>
+     </html>
+     </>
+   )
+ }
+}
--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return ( <p>not done yet</p>)
-}
-
-export default App
