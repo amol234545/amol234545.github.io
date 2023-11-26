41729574f4389944b7f78d181a78114a1cb7c254
amol234545 <amolmilton@gmail.com>
2023-11-26 13:21:49 -0500

    counter

--- pages/index$.tsx
+++ pages/index$.tsx
@@ -2,8 +2,11 @@
  * @title Hello
  */
 import React from "react";
+import {useState} from "react";
+import { Button,Card } from 'react-bootstrap';
 
 export default function index() {
+   const [count,setCount] = useState(0)
    return (
      <>
      <html>
@@ -12,7 +15,20 @@ export default function index() {
         </head>
         <body>
             <div>
-                
+                <Card>
+                  <Card.Header>
+                    Hello
+                  </Card.Header>
+                  <Card.Body>
+                  <Card.Title>
+                    Button Counter
+                  </Card.Title>
+                  <Card.Text>
+                    Count:${count}
+                  </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Button variant="dark" onClick={() => {setCount(count + 1)}}>Click</Button>
             </div>
         </body>
      </html>
